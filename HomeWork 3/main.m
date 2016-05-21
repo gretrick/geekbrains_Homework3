49fe814152326a89d2fa72a7ee1f17f34b081b3f
Sergejs Lastovskis <Sergejs.lastovskis@gmail.com>
2016-05-21 14:42:39 +0300

    Сделанное ДЗ.

--- HomeWork 3/main.m
+++ HomeWork 3/main.m
@@ -8,10 +8,61 @@
 
 #import <Foundation/Foundation.h>
 
+typedef enum {
+    plus,
+    minus,
+    multiply,
+    division
+} CalculationType;
+
+int calc(int firstNumber, int secondNumber, CalculationType type){
+    switch (type) {
+        case plus:
+            return firstNumber + secondNumber;
+        case minus:
+            return firstNumber - secondNumber;
+        case multiply:
+            return firstNumber * secondNumber;
+        case division:
+            return firstNumber / secondNumber;
+    }
+    return 0;
+}
+
+int quadMultiplier(int number){
+    
+    return number * 4;
+}
+
+int factorial(int number){
+    int r = 1;
+    for (int i = 2; i <= number; ++i)
+        r = r * i;
+    return r;
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        int firstNumber = 11;
+        int secondNumber = 33;
+        NSLog(@"Результат %d + %d равен %d", firstNumber, secondNumber, calc(firstNumber,secondNumber,0));
+        NSLog(@"Результат %d - %d равен %d", firstNumber, secondNumber, calc(firstNumber,secondNumber,1));
+        NSLog(@"Результат %d * %d равен %d", firstNumber, secondNumber, calc(firstNumber,secondNumber,2));
+        NSLog(@"Результат %d / %d равен %d", firstNumber, secondNumber, calc(firstNumber,secondNumber,3));
+        
+        int operations = 20;
+        for (int i=1 ; i<=operations ; i++){
+            if (quadMultiplier(i) == 16) break;
+            if (quadMultiplier(i) % 3 == 0){
+                NSLog(@"Число %d кратно 3", i);
+                continue;
+            }
+            NSLog(@"Осталось %d итераций" , operations-i);
+            
+        }
+        NSLog(@"Факториал от 10 = %d", factorial(10));
+        
     }
     return 0;
 }
